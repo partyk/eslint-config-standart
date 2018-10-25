88643c83a1a71fbfb051accac81631248aacbec9
Bohdan Partyk <info@podebrady.org>
2018-10-25 14:55:46 +0200

    up to ecmaVersion 7

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
         "es6": true
     },
     "parserOptions": {
-        "ecmaVersion": 6,
+        "ecmaVersion": 7,
         "sourceType": "module",
         "ecmaFeatures": {
         }
